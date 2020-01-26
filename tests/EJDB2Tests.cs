66fc1260fc34621d3b5de21dc2853a2a43881019
kmvi <kmvi@users.noreply.github.com>
2020-01-26 10:22:17 +0300

    add tests

---
+++ tests/EJDB2Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Ejdb2.Tests
+{
+    public class EJDB2Tests
+    {
+        [Fact]
+        public void Ctor_NullArgument_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new EJDB2(null));
+        }
+    }
+}
