fe389345ce994f1d745d8a4599067cb434f9c6b7
kmvi <kmvi@users.noreply.github.com>
2020-01-26 10:21:00 +0300

    add native helper

---
+++ src/Native/INativeHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejdb2.Native
+{
+    internal interface INativeHelper
+    {
+        ulong ejdb_init();
+        ulong ejdb_open(ref EJDB_OPTS opts, out IntPtr ejdbp);
+        ulong ejdb_close(ref IntPtr ejdbp);
+        ulong ejdb_get_meta(IntPtr handle, out IntPtr jblp);
+        ulong jbl_from_json(out IntPtr jblp, string jsonstr);
+        ulong jbl_as_json(IntPtr jbl, jbl_json_printer pt, IntPtr op, jbl_print_flags_t pf);
+        ulong ejdb_put(IntPtr db, string coll, IntPtr jbl, long id);
+        ulong ejdb_put_new(IntPtr db, string coll, IntPtr jbl, out long oid);
+        void jbl_destroy(ref IntPtr jblp);
+        ulong ejdb_del(IntPtr db, string coll, long id);
+        ulong ejdb_get(IntPtr db, string coll, long id, out IntPtr jblp);
+        ulong ejdb_rename_collection(IntPtr db, string coll, string new_coll);
+        ulong ejdb_merge_or_put(IntPtr db, string coll, string patchjson, long id);
+        ulong ejdb_patch(IntPtr db, string coll, string patchjson, long id);
+        ulong ejdb_ensure_index(IntPtr db, string coll, string path, ejdb_idx_mode_t mode);
+        ulong ejdb_remove_index(IntPtr db, string coll, string path, ejdb_idx_mode_t mode);
+        ulong ejdb_online_backup(IntPtr db, out ulong ts, string target_file);
+        ulong ejdb_remove_collection(IntPtr db, string coll);
+        int ejdb_version_major();
+        int ejdb_version_minor();
+        int ejdb_version_patch();
+    }
+}
---
+++ src/Native/Win32/Win32NativeHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejdb2.Native.Win32
+{
+    internal sealed class Win32NativeHelper : INativeHelper
+    {
+        public ulong ejdb_close(ref IntPtr ejdbp) => Interop.ejdb_close(ref ejdbp);
+
+        public ulong ejdb_del(IntPtr db, string coll, long id) => Interop.ejdb_del(db, coll, id);
+
+        public ulong ejdb_ensure_index(IntPtr db, string coll, string path, ejdb_idx_mode_t mode) => Interop.ejdb_ensure_index(db, coll, path, mode);
+
+        public ulong ejdb_get(IntPtr db, string coll, long id, out IntPtr jblp) => Interop.ejdb_get(db, coll, id, out jblp);
+
+        public ulong ejdb_get_meta(IntPtr db, out IntPtr jblp) => Interop.ejdb_get_meta(db, out jblp);
+
+        public ulong ejdb_init() => Interop.ejdb_init();
+
+        public ulong ejdb_merge_or_put(IntPtr db, string coll, string patchjson, long id) => Interop.ejdb_merge_or_put(db, coll, patchjson, id);
+
+        public ulong ejdb_online_backup(IntPtr db, out ulong ts, string target_file) => Interop.ejdb_online_backup(db, out ts, target_file);
+
+        public ulong ejdb_open(ref EJDB_OPTS opts, out IntPtr ejdbp) => Interop.ejdb_open(ref opts, out ejdbp);
+
+        public ulong ejdb_patch(IntPtr db, string coll, string patchjson, long id) => Interop.ejdb_patch(db, coll, patchjson, id);
+
+        public ulong ejdb_put(IntPtr db, string coll, IntPtr jbl, long id) => Interop.ejdb_put(db, coll, jbl, id);
+
+        public ulong ejdb_put_new(IntPtr db, string coll, IntPtr jbl, out long oid) => Interop.ejdb_put_new(db, coll, jbl, out oid);
+
+        public ulong ejdb_remove_collection(IntPtr db, string coll) => Interop.ejdb_remove_collection(db, coll);
+
+        public ulong ejdb_remove_index(IntPtr db, string coll, string path, ejdb_idx_mode_t mode) => Interop.ejdb_remove_index(db, coll, path, mode);
+
+        public ulong ejdb_rename_collection(IntPtr db, string coll, string new_coll) => Interop.ejdb_rename_collection(db, coll, new_coll);
+
+        public int ejdb_version_major() => Interop.ejdb_version_major();
+
+        public int ejdb_version_minor() => Interop.ejdb_version_minor();
+
+        public int ejdb_version_patch() => Interop.ejdb_version_patch();
+
+        public ulong jbl_as_json(IntPtr jbl, jbl_json_printer pt, IntPtr op, jbl_print_flags_t pf) => Interop.jbl_as_json(jbl, pt, op, pf);
+
+        public void jbl_destroy(ref IntPtr jblp) => Interop.jbl_destroy(ref jblp);
+
+        public ulong jbl_from_json(out IntPtr jblp, string jsonstr) => Interop.jbl_from_json(out jblp, jsonstr);
+    }
+}
