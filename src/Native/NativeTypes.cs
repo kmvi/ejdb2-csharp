6a86ee8352fb724dd3d3ecb3f2eb3fa6d77b336e
kmvi <kmvi@users.noreply.github.com>
2020-01-26 10:20:21 +0300

    add windows native methods

---
+++ src/Native/NativeTypes.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ejdb2.Native
+{
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    internal delegate ulong jbl_json_printer(
+        [MarshalAs(UnmanagedType.LPStr)] string data, int size,
+        char ch, int count, IntPtr op);
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct EJDB_OPTS
+    {
+        public IWKV_OPTS kv;
+        public EJDB_HTTP http;
+        public bool no_wal;
+        public uint sort_buffer_sz;
+        public uint document_buffer_sz;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct EJDB_HTTP
+    {
+        public bool enabled;
+        public int port;
+        public string bind;
+        public string access_token;
+        public UIntPtr access_token_len;
+        public bool blocking;
+        public bool read_anon;
+        public UIntPtr max_body_size;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct IWKV_OPTS
+    {
+        public string path;
+        public uint random_seed;
+        public iwkv_openflags oflags;
+        public bool file_lock_fail_fast;
+        public IWKV_WAL_OPTS wal;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct IWKV_WAL_OPTS
+    {
+        public bool enabled;
+        public bool check_crc_on_checkpoint;
+        public uint savepoint_timeout_sec;
+        public uint checkpoint_timeout_sec;
+        public UIntPtr wal_buffer_sz;
+        public ulong checkpoint_buffer_sz;
+        public IntPtr wal_lock_interceptor;
+        public IntPtr wal_lock_interceptor_opaque;
+    }
+
+    [Flags]
+    internal enum iwkv_openflags : byte
+    {
+        IWKV_RDONLY = 0x02,
+        IWKV_TRUNC = 0x04,
+    }
+
+    [Flags]
+    internal enum jbl_print_flags_t : byte
+    {
+        JBL_PRINT_NONE = 0,
+        JBL_PRINT_PRETTY = 0x01,
+        JBL_PRINT_CODEPOINTS = 0x02,
+    }
+
+    [Flags]
+    internal enum ejdb_idx_mode_t : byte
+    {
+        EJDB_IDX_UNIQUE = 0x01,
+        EJDB_IDX_STR = 0x04,
+        EJDB_IDX_I64 = 0x08,
+        EJDB_IDX_F64 = 0x10,
+    }
+}
---
+++ src/Native/Win32/Interop.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ejdb2.Native.Win32
+{
+    internal static class Interop
+    {
+        public const string LibName = "libejdb2";
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern uint ejdb_init();
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int ejdb_version_major();
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int ejdb_version_minor();
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int ejdb_version_patch();
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern ulong ejdb_open([In] ref EJDB_OPTS opts, [Out] out IntPtr ejdbp);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern ulong ejdb_close([In, Out] ref IntPtr ejdbp);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern ulong ejdb_get_meta([In] IntPtr db, [Out] out IntPtr jblp);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong jbl_as_json(IntPtr jbl,
+            [MarshalAs(UnmanagedType.FunctionPtr)] jbl_json_printer pt,
+            IntPtr op, jbl_print_flags_t pf);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong jbl_from_json([Out] out IntPtr jblp,
+            [MarshalAs(UnmanagedType.LPStr)] string jsonstr);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong ejdb_put(IntPtr db,
+            [MarshalAs(UnmanagedType.LPStr)] string coll, IntPtr jbl, long id);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong ejdb_put_new(IntPtr db,
+            [MarshalAs(UnmanagedType.LPStr)] string coll, IntPtr jbl, [Out] out long oid);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void jbl_destroy([In, Out] ref IntPtr jblp);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong ejdb_del(IntPtr db,
+            [MarshalAs(UnmanagedType.LPStr)] string coll, long id);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong ejdb_get(IntPtr db,
+            [MarshalAs(UnmanagedType.LPStr)] string coll, long id, [Out] out IntPtr jblp);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong ejdb_rename_collection(IntPtr db,
+            [MarshalAs(UnmanagedType.LPStr)] string coll, [MarshalAs(UnmanagedType.LPStr)] string new_coll);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong ejdb_merge_or_put(IntPtr db,
+            [MarshalAs(UnmanagedType.LPStr)] string coll,
+            [MarshalAs(UnmanagedType.LPStr)] string patchjson, long id);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong ejdb_patch(IntPtr db,
+            [MarshalAs(UnmanagedType.LPStr)] string coll,
+            [MarshalAs(UnmanagedType.LPStr)] string patchjson, long id);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong ejdb_ensure_index(IntPtr db,
+            [MarshalAs(UnmanagedType.LPStr)] string coll,
+            [MarshalAs(UnmanagedType.LPStr)] string path, ejdb_idx_mode_t mode);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong ejdb_remove_index(IntPtr db,
+            [MarshalAs(UnmanagedType.LPStr)] string coll,
+            [MarshalAs(UnmanagedType.LPStr)] string path, ejdb_idx_mode_t mode);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong ejdb_online_backup(IntPtr db,
+            [Out] out ulong ts, [MarshalAs(UnmanagedType.LPStr)] string target_file);
+
+        [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern ulong ejdb_remove_collection(IntPtr db, [MarshalAs(UnmanagedType.LPStr)] string coll);
+    }
+}
