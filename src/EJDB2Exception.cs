62fb92db03ecbb9c407605f7ef40ee7901a6d644
kmvi <kmvi@users.noreply.github.com>
2020-01-26 10:21:13 +0300

    add EJDB2Exception

---
+++ src/EJDB2Exception.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Ejdb2
+{
+    [Serializable]
+    public class EJDB2Exception : Exception
+    {
+        public EJDB2Exception(ulong code)
+        {
+            Code = code;
+        }
+
+        public EJDB2Exception(ulong code, string message)
+            : base(message)
+        {
+            Code = code;
+        }
+
+        public EJDB2Exception(ulong code, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            Code = code;
+        }
+
+        protected EJDB2Exception(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+
+        public ulong Code { get; }
+    }
+}
